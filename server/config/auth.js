679c05f1befa677a75e9e0be7c6ce7303fc6e3aa
emekaogb <124109191+emekaogb@users.noreply.github.com>
2026-04-29 18:09:20 -0400

    state mismatch fix

--- server/config/auth.js
+++ server/config/auth.js
@@ -2,12 +2,19 @@ import { betterAuth } from "better-auth";
 import { pool } from "../config/database.js"
 import "dotenv/config";
 
+const isProd = process.env.NODE_ENV === "production"
+
 export const auth = betterAuth({
   database: pool,
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   trustedOrigins: ["http://localhost:5173", process.env.CLIENT_URL].filter(Boolean),
-
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: isProd ? "none" : "lax",
+      secure: isProd,
+    },
+  },
   socialProviders: {
     google: {
       prompt: "select_account",
